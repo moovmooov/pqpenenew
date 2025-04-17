c638c067500a39cf23da477396db036b60200f0d
Mateus Vasconcelos <mateusmoov@gmail.com>
2025-04-17 00:51:40 -0300

    data

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,8 @@
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
+import { useStore } from "../store/useStore"
+
 export default function Footer() {
+  const { databaseData } = useStore()
   return (
     <footer className="p-4">
       <p className="text-center text-[12px] font-light text-gray-900 dark:text-gray-300">São válidas apenas partidas com no mínimo 3 <TooltipProvider>
@@ -13,7 +16,7 @@ export default function Footer() {
             <p>Moov, Tomahawk, Madrugadão, Nervous Bear, Bixo Preto ou Callegari.</p>
           </TooltipContent>
         </Tooltip>
-      </TooltipProvider> (contando com o próprio penenew) e a partir da data 06/05/2025.</p>
+      </TooltipProvider> (contando com o próprio penenew) e a partir da data {new Date(databaseData?.matches[0].date).toLocaleDateString('pt-BR')}.</p>
     </footer>
   )
 }
